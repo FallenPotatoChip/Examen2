d0de793f57cecf1dc3c0388ff07a92435a4f4e70
alulab14 <alulab14@inf.pucp.edu.pe>
2018-07-06 17:39:49 -0500

    AAA

--- pucp-examen2-0e6a8380eca1/app/src/main/java/pe/edu/pucp/examen2/View/Pantalla3.java
+++ pucp-examen2-0e6a8380eca1/app/src/main/java/pe/edu/pucp/examen2/View/Pantalla3.java
@@ -3,11 +3,19 @@ package pe.edu.pucp.examen2.View;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import pe.edu.pucp.examen2.Presenter.UserPresenter;
+
 public class Pantalla3 extends AppCompatActivity {
 
+    private UserPresenter presenter
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla3);
+        presenter = new UserPresenter(this);
+        setupFields();
+
+        presenter.getUsers();
+
     }
 }
